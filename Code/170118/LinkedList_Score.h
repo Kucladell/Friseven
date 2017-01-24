a7a216d7626f5415076c880682f99b5a57cf3a28
Kucladell <kucladell@gmail.com>
2017-01-25 01:31:45 +0900

    Created 170118

---
+++ Code/170118/LinkedList_Score.h
@@ -0,0 +1,33 @@
+#pragma once
+
+struct Score
+{
+	std::string name;
+	int credit;
+	float grade;
+	std::string gradeText;
+	bool isMajor;
+};
+
+struct Node
+{
+	Score value;
+	Node* next;
+};
+
+class LinkedList
+{
+public:
+	LinkedList();
+	~LinkedList();
+
+	void add(); //새 값을 입력
+	void print(); //모든 노드를 출력
+	void calculate(); //학점 계산 결과를 출력
+	void clear(); //모든 노드를 삭제
+
+private:
+	Node* head;
+	int count; //노드의 개수
+
+};
